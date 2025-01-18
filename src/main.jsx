605965c8b86274c512bcd0ba68f4ebcb9cb5029c
lidiapdiaz <lidiapd15@hotmail.com>
2025-01-18 21:04:02 +0100

    empezamos a crear y estilar el primer componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './app/App'
+import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  "ola mundo"
-  </StrictMode>,
+    <App/>
+  </StrictMode>
 )
