fe2d91800568ee9098f611a23e944ef9cd719d0f
lidiapdiaz <lidiapd15@hotmail.com>
2025-01-18 21:04:02 +0100

    empezamos a crear y estilar el primer componente

--- Twitter-card/src/main.jsx
+++ Twitter-card/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< HEAD:Twitter-card/src/main.jsx
 import App from './App'
+=======
+import App from './app/App'
+>>>>>>> 605965c (empezamos a crear y estilar el primer componente):src/main.jsx
 import './index.css'
 
 
