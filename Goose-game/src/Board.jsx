4e3f507e6a8ce8c6df1fae38eb2a598b41e4c1c1
lidiapdiaz <lidiapd15@hotmail.com>
2025-01-26 16:31:23 +0100

    creamos un tablero(board) de prueba

--- Goose-game/src/Board.jsx
+++ Goose-game/src/Board.jsx
@@ -1,11 +1,17 @@
 import Dice from "./Dice"
 
+//Creamos un array de 10 posiciones que serán las casillas
+const board = Array(15).fill(null)
+
 function Board() {
 
   return (
     <>
     <h1>Juego de la oca</h1>
     <Dice/>
+    {board.map((cell, index) =>{
+      return(<div key={index}>{index}</div>)
+    })}
     </>
   )
 }
--- Goose-game/src/Dice.jsx
+++ Goose-game/src/Dice.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
 
-function Dice() {
-//Crear estado, cada vez que se tire el dado, aparezca el resultado
-const [rollDice, setRollDice] = useState(1);
 
-const handleRollDice = () =>{
+function Dice() {
+    //Crear estado, cada vez que se tire el dado, aparezca el resultado
+    const [rollDice, setRollDice] = useState(1); //iniciamos con el num 1
+
+    //Función para generart la tirada del dado
+    const handleRollDice = () =>{
     const numberDice =  Math.floor(Math.random() * 6) + 1;
     setRollDice(numberDice); 
 }
-
 
 return (
     <>
