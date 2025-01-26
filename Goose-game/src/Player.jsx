85bae134725ddb63c65fb83f20cc1b1fce059223
lidiapdiaz <lidiapd15@hotmail.com>
2025-01-26 19:06:40 +0100

    he probado cómo se podría mover la ficha del jugador pero hay que depurarlo

--- Goose-game/src/Player.jsx
+++ Goose-game/src/Player.jsx
@@ -1,8 +1,11 @@
-//Ficha de xogador
+//Ficha de xogador 
+// <div className={`text-3xl relative left-[${move}px]`}>♟️</div>
+
+const move = "left-[240px]";//así si que funciona, cambiando 'manualmente'
 
 function Player() {
   return (
-    <div className="text-3xl">♟️</div>
+    <div className={`text-5xl relative ${move}`}>♟️</div>
   )
 }
 
