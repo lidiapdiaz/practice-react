01c47410c3c2dfd54ccf471a29016edae901f9aa
lidiapdiaz <lidiapd15@hotmail.com>
2025-05-02 14:42:00 +0200

    Se muestran tarjetas dioses con estilo final aplicado

--- ecommerce/src/components/Gods.jsx
+++ ecommerce/src/components/Gods.jsx
@@ -1,22 +1,23 @@
+import './gods.css'
+
 export default function Gods({ gods }) {
 
   return (
-    <main>
+    <main className="gods">
       <ul>
         {gods.map(god => (
           <li key={god.id}>
             <img
               src={god.url}
               alt={god.name}
-              width={200}
             />
             <div >
-              <h3>{god.name}</h3>
+              <h3>{god.name} <span> - {god.hp}&#9877;</span> </h3>
               <p>{god.mithology}</p>
             </div>
             <div>
               <button>
-                Add to party
+              add to party
               </button>
             </div>
           </li>
