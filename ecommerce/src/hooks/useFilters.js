5b28ae9013f57113b1a01fcd4d1d9e50a470ac91
lidiapdiaz <lidiapd15@hotmail.com>
2025-05-03 16:54:54 +0200

    Metemos la lógica de filtros en un custom hook

---
+++ ecommerce/src/hooks/useFilters.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function useFilters() {
+  //estado para crear filtros
+    const [filters, setFilters] = useState({
+    mithology: 'all',
+    minHp: 0
+    })
+
+    //Creamos unha función para filtrar deuses
+    const filterGods = (gods) => {
+      //usamos metodo filter para filtra-lo array dos deuses
+    return gods.filter(god => {
+        return(//por cada elemento/deus (god) quero que me devolvas:
+          god.hp >= filters.minHp && //se o campo hp é igual que o elexido no filtro
+            (
+            filters.mithology === 'all' || // se no filtro teño elexida mitoloxía todas
+            god.mithology === filters.mithology //se coincide co filtro de mitoloxia seleccionado
+            )
+        )})
+    }
+return {filterGods, setFilters}
+}
+
+export default useFilters;
